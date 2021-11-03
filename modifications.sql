e928af83125429c2e78a8480d144c265a2396910
petumazo <davidalvarezmazzo@gmail.com>
2021-11-03 12:29:56 -0500

    Add commands to modifications.sql file.

--- modifications.sql
+++ modifications.sql
@@ -4,5 +4,11 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 -- Create an index on visits(animal_id) to improve 1st query performance
 CREATE INDEX idx_animal_id ON visits(animal_id);
 
+-- Create an index on visits(vet_id) to improve 2nd query performance
+CREATE INDEX idx_vet_id ON visits(vet_id);
+
+-- Cluster visits table using the index
+CLUSTER visits USING idx_vet_id;
+
 -- Create an index on owners(email) to improve 3rd query performance
 CREATE INDEX idx_email ON owners(email);
